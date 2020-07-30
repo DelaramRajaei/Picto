50ae895679bbb87bc838bcaa5b6c9702cccecf40
DelaramRajaei <delaram.rajaei@yahoo.com>
2020-07-30 19:47:09 +0430

    Initial commit

---
+++ src/DBConnection.java
@@ -0,0 +1,82 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class DBConnection {
+
+    private Connection con;
+    private Statement st;
+    private ResultSet rs;
+    private ResultSetMetaData metadata;
+
+    public DBConnection() {
+
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
+            st = con.createStatement();
+
+        } catch (Exception e) {
+            System.out.println("Error :" + e);
+        }
+
+    }
+
+    public void getData(String tableName) {
+        try {
+            rs = st.executeQuery("select * from" + " " + tableName);
+            metadata = rs.getMetaData();
+            int numberOfColumns = metadata.getColumnCount();
+
+            while (rs.next()) {
+
+                for (int i = 1; i <= numberOfColumns; i++) {
+                    System.out.print(metadata.getColumnName(i) + ": " + rs.getString(i) + " , ");
+                }
+                System.out.println();
+            }
+        } catch (
+                Exception e) {
+            System.out.println("Error :" + e);
+        }
+    }
+
+    public void addData(String tableName) {
+        Scanner scan=new Scanner(System.in);
+        String sql=new String();
+        try {
+            rs = st.executeQuery("select * from" + " " + tableName);
+            metadata = rs.getMetaData();
+            int numberOfColumns = metadata.getColumnCount();
+
+            while (rs.next()) {
+                for (int i = 1; i <= numberOfColumns; i++) {
+
+                    System.out.print("Plz enter" + metadata.getColumnName(i) + ":");
+                    sql=scan.next()+", "+sql;
+                }
+
+            }
+        } catch (Exception e) {
+            System.out.println("Error :" + e);
+        }
+    }
+
+    public void updateData(String tableName) {
+    }
+
+    public void deleteData(String tableName) {
+    }
+
+    public boolean checkTable(String tableName) {
+        boolean exist = false;
+        try {
+            DatabaseMetaData meta = con.getMetaData();
+            rs = meta.getTables(null, null, tableName, null);
+            if (rs.next()) exist = true;
+        } catch (Exception e) {
+            System.out.println("Error :" + e);
+        }
+        return exist;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        DBConnection connection = new DBConnection();
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+        boolean exist = false;
+        String tableName=null;
+        while (flag) {
+
+            System.out.println("Plz choose an option:\n1.Show a table\n2.Add a record\n3.Update a record\n4.Delete a record\n5.Exit");
+            int option = sc.nextInt();
+
+            if (option!=5)System.out.println("Plz enter your table's name:");
+
+            while (!exist) {
+                tableName = sc.next();
+                exist = connection.checkTable(tableName);
+                if (!exist) System.out.println("This table doesn't exist plz enter another one:");
+            }
+
+
+            switch (option) {
+                case 1:
+                    connection.getData(tableName);
+                    break;
+
+                case 2:
+                    connection.addData(tableName);
+                    break;
+
+                case 3:
+                    connection.updateData(tableName);
+                    break;
+
+                case 4:
+                    connection.deleteData(tableName);
+                    break;
+
+                case 5:
+                    flag = false;
+                    break;
+
+            }
+        }
+
+    }
+}
